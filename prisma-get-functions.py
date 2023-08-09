42ffccb19db5864aca3bf56482f70ed805bcfe7e
Tim Lloyd CCIE #42804 <97355075+Timothy-Lloyd@users.noreply.github.com>
2023-08-09 16:23:19 +0100

    Create prisma-get-functions.py

---
+++ prisma-get-functions.py
@@ -0,0 +1,13 @@
+from defs import *
+
+#List the candidate configurations.
+#list_candidate()
+
+#Get a configuration by version.
+#get_config()
+
+#Get the running configuration.
+#get_running_config()
+
+#Get list of security rules.
+get_mu_post_security_rules()
