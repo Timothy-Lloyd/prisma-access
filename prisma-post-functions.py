d640e6b9a1c58fc5516020fb5c19c15d56201600
Tim Lloyd CCIE #42804 <97355075+Timothy-Lloyd@users.noreply.github.com>
2024-01-03 14:07:24 +0000

    Update prisma-post-functions.py

--- prisma-post-functions.py
+++ prisma-post-functions.py
@@ -1,1 +1,13 @@
+from defs import *
 
+### Post list of security rules.
+#put_shared_post_security_rules()
+
+### Post the global URL access profiles
+#put_global_url_access_profiles()
+
+### Post the global file blocking profiles
+#put_global_file_blocking_profiles()
+
+### Posts the services defined within objects
+#put_global_objects_services()
