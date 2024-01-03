a228e5b07850ce6314470c3a5ffa613594a057ae
Tim Lloyd CCIE #42804 <97355075+Timothy-Lloyd@users.noreply.github.com>
2024-01-03 14:03:54 +0000

    Update defs.py with more functions

--- defs.py
+++ defs.py
@@ -1,6 +1,13 @@
 import requests
 import vars
 import json
+import time
+import os
+import json
+
+localtime = time.localtime()
+formattime = time.strftime("%d-%m-%Y %H:%M:%S", localtime)
+datetime = time.strftime("%d-%m-%Y", localtime)
 
 def get_token():
     global token
@@ -29,6 +36,10 @@ headers = {
 'Accept': 'application/json',
 'Authorization': 'Bearer ' + token
 }
+headersPost = {
+'Content-Type': 'application/json',
+'Authorization': 'Bearer ' + token
+}
 
 def get_running_config():
     url = "https://api.sase.paloaltonetworks.com/sse/config/v1/config-versions/running"
@@ -46,12 +57,67 @@ def list_candidate():
     response = requests.request("GET", url, headers=headers, data=payload)
     print(response.text)
 
-def get_mu_post_security_rules():
-    url = "https://api.sase.paloaltonetworks.com/sse/config/v1/security-rules?position=pre&folder=Mobile Users"
+def get_shared_post_security_rules():
+    url = "https://api.sase.paloaltonetworks.com/sse/config/v1/security-rules?position=post&folder=Shared"
     response = requests.request("GET", url, headers=headers, data=payload)
     print(response.text)
+    with open(os.path.join("output/shared_post_security_rules.json"), "a") as file:
+        json.dump(response.json(), file)
+def put_shared_post_security_rules():
+    url = "https://api.sase.paloaltonetworks.com/sse/config/v1/security-rules?position=post&folder=Shared"
+    with open(os.path.join("output/shared_post_security_rules.json"), "r") as file:
+        payload = json.load(file)
+    response = requests.request("POST", url, headers=headersPost, json=payload)
+    print(response.text)
+
+def get_global_url_access_profiles():
+    url = "https://api.sase.paloaltonetworks.com/sse/config/v1/url-access-profiles?folder=Shared"
+    response = requests.request("GET", url, headers=headers, data=payload)
+    print(response.text)
+    with open(os.path.join("output/global_url_access_profile.json"), "a") as file:
+        json.dump(response.json(), file)
+def put_global_url_access_profiles():
+    url = "https://api.sase.paloaltonetworks.com/sse/config/v1/url-access-profiles?folder=All"
+    with open(os.path.join("output/global_url_access_profile.json"), "r") as file:
+        payload = json.load(file)
+    response = requests.request("POST", url, headers=headersPost, json=payload)
+    print(response.text)
+ 
+def get_global_file_blocking_profiles():
+    url = "https://api.sase.paloaltonetworks.com/sse/config/v1/file-blocking-profiles?folder=Shared"
+    response = requests.request("GET", url, headers=headers, data=payload)
+    print(response.text)
+    with open(os.path.join("output/global_file_blocking_profile.json"), "a") as file:
+        json.dump(response.json(), file)
+def put_global_file_blocking_profiles():
+    url = "https://api.sase.paloaltonetworks.com/sse/config/v1/file-blocking-profiles?folder=All"
+    with open(os.path.join("output/global_file_blocking_profile.json"), "r") as file:
+        payload = json.load(file)
+    response = requests.request("POST", url, headers=headersPost, json=payload)
+    print(response.text)
+ 
+def get_global_objects_services():
+    url = "https://api.sase.paloaltonetworks.com/sse/config/v1/services?folder=Shared"
+    response = requests.request("GET", url, headers=headers, data=payload)
+    print(response.text)
+    with open(os.path.join("output/global_objects_services.json"), "a") as file:
+        json.dump(response.json(), file)
+def put_global_objects_services():
+    url = "https://api.sase.paloaltonetworks.com/sse/config/v1/services?folder=All"
+    with open(os.path.join("output/global_objects_services.json"), "r") as file:
+        payload = json.load(file)
+    response = requests.request("POST", url, headers=headersPost, json=payload)
+    print(response.text)
+
 
 #list_candidate()
 #get_config()
 #get_running_config()
-#get_mu_post_security_rules()
+#get_shared_post_security_rules()
+#put_shared_post_security_rules()
+#get_global_url_access_profiles()
+#put_global_url_access_profiles()
+#get_global_file_blocking_profiles()
+#put_global_file_blocking_profiles()
+#get_global_objects_services()
+#put_global_objects_services()
