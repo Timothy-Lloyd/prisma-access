9a97ce43c93f0fd67ba54ac51730b1432f2d8bd6
Tim Lloyd CCIE #42804 <97355075+Timothy-Lloyd@users.noreply.github.com>
2024-01-03 14:06:27 +0000

    Update prisma-get-functions.py

--- prisma-get-functions.py
+++ prisma-get-functions.py
@@ -1,13 +1,22 @@
 from defs import *
 
-#List the candidate configurations.
+### List the candidate configurations.
 #list_candidate()
 
-#Get a configuration by version, currently set as version 33.
+### Get a configuration by version, currently set as version 33.
 #get_config()
 
-#Get the running configuration.
+### Get the running configuration.
 #get_running_config()
 
-#Get list of security rules.
-get_mu_post_security_rules()
+### Get list of security rules.
+#get_shared_post_security_rules()
+
+### Get the global URL access profiles
+#get_global_url_access_profiles()
+
+### Get the global file blocking profiles
+#get_global_file_blocking_profiles()
+
+### Gets the services defined within objects
+#get_global_objects_services()
